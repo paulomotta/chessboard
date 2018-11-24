45c91c7676e70df7cc89b64348ec111c5992bba7
Paulo Motta <prmottajr@gmail.com>
2018-11-24 08:33:52 -0200

    Renamed the test class since there will be different parameters for different tests
    --- ChessboardSplitColumnRowTest.java ---
    Renamed the class
    Added more test cases

---
+++ poc/src/test/java/br/pro/paulomotta/poc/ChessboardSplitColumnRowTest.java
@@ -0,0 +1,65 @@
+package br.pro.paulomotta.poc;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author paulo
+ */
+@RunWith(Parameterized.class)
+public class ChessboardSplitColumnRowTest {
+
+    String position;
+    String[] expResult;
+
+    public ChessboardSplitColumnRowTest(String position, String[] result) {
+        this.position = position;
+        this.expResult = result;
+    }
+
+    @Parameterized.Parameters
+    public static Collection positions() {
+        return Arrays.asList(new Object[][]{
+            {"a1", new String[]{"a", "1"}},
+            {"aa1", new String[]{"aa", "1"}},
+            {"aa11", new String[]{"aa", "11"}},
+            {"a11", new String[]{"a", "11"}},
+            {"a", null},
+            {"1", null},
+        });
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testPositions() {
+        System.out.println("Position is : " + position + " expResult=" + expResult);
+        Chessboard instance = new Chessboard();
+
+        if (expResult == null) {
+            try {
+                String[] result = instance.splitColumnRow(position);
+                fail("Where is the exception?");
+            } catch (IllegalArgumentException ex) {
+                //ok
+            }
+        } else {
+            assertArrayEquals(expResult, instance.splitColumnRow(position));
+        }
+    }
+
+}
