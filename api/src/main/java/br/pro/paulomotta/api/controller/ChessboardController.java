6bb28086c4eb42d427b072c4318ac51b8cd4954b
Paulo Motta <prmottajr@gmail.com>
2018-11-27 13:26:02 -0200

    Fixes #4 Repository autowiring and usage
    --- ChessboardController.java ---
    Autowired the repository
    Used the repository in all methods to persist the request strings and the result
    The add/remove column/row and reset methods had to be refactored to retrieve the value before returning it

--- api/src/main/java/br/pro/paulomotta/api/controller/ChessboardController.java
+++ api/src/main/java/br/pro/paulomotta/api/controller/ChessboardController.java
@@ -1,11 +1,14 @@
 package br.pro.paulomotta.api.controller;
 
 import br.pro.paulomotta.api.model.PositionDTO;
+import br.pro.paulomotta.api.model.Request;
+import br.pro.paulomotta.api.repository.RequestRepository;
 import br.pro.paulomotta.poc.Chessboard;
 import br.pro.paulomotta.poc.Position;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ChessboardController {
     Chessboard board = new Chessboard();
+    
+    @Autowired
+    RequestRepository repo;
     
     @GetMapping("/moves/{piece}/{start}/{turns}")
     @ResponseBody
@@ -32,6 +38,8 @@ public class ChessboardController {
         for (Position position : calculated) {
             moves.add(new PositionDTO(Position.positionToAlgebraic(position)));
         }
+        Request req = new Request(start+"/"+piece+"/"+turns,moves.toString());
+        repo.save(req);
         
         return moves;
     }
@@ -39,30 +47,45 @@ public class ChessboardController {
     @PostMapping("/add/column")
     @ResponseBody
     public Boolean addColumn(){
-        return board.addColumn();
+        Boolean b = board.addColumn();
+        Request req = new Request("/add/column",b.toString());
+        repo.save(req);
+        return b;
     }
     
     @PostMapping("/remove/column")
     @ResponseBody
     public Boolean removeColumn(){
-        return board.removeColumn();
+        Boolean b = board.removeColumn();
+        Request req = new Request("/remove/column",b.toString());
+        repo.save(req);
+        return b;
     }
     
     @PostMapping("/add/row")
     @ResponseBody
     public Boolean addRow(){
-        return board.addRow();
+        Boolean b = board.addRow();
+        Request req = new Request("/add/row",b.toString());
+        repo.save(req);
+        return b;
     }
     
     @PostMapping("/remove/row")
     @ResponseBody
     public Boolean removeRow(){
-        return board.removeRow();
+        Boolean b = board.removeRow();
+        Request req = new Request("/remove/row",b.toString());
+        repo.save(req);
+        return b;
     }
     
     @PostMapping("/reset")
     @ResponseBody
     public Boolean reset(){
-        return board.reset();
+        Boolean b = board.reset();
+        Request req = new Request("/reset",b.toString());
+        repo.save(req);
+        return b;
     }
 }
