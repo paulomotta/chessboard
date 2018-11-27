cb3aedccdefeca844912482bd26dc32df4dd825b
Paulo Motta <prmottajr@gmail.com>
2018-11-27 13:21:28 -0200

    Part #4 Added the repository interface
    --- RequestRepository.java ---
    A repository interface for autowiring

---
+++ api/src/main/java/br/pro/paulomotta/api/repository/RequestRepository.java
@@ -0,0 +1,13 @@
+package br.pro.paulomotta.api.repository;
+
+import br.pro.paulomotta.api.model.Request;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author paulo
+ */
+@Repository
+public interface RequestRepository extends CrudRepository<Request, Long>{
+}
