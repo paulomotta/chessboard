4a867067e96a36ac60afc9cf60c448666c3efb02
Paulo Motta <prmottajr@gmail.com>
2018-11-27 13:20:50 -0200

    Part #4 Added the request entity to persist the request and the result
    --- Request.java ---
    An Entity to save the pair request string/ result string (if a list it will be serialized)

---
+++ api/src/main/java/br/pro/paulomotta/api/model/Request.java
@@ -0,0 +1,34 @@
+package br.pro.paulomotta.api.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author paulo
+ */
+@Entity
+public class Request implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String request;
+    private String result;
+
+    protected Request() {}
+
+    public Request(String request, String result) {
+        this.request = request;
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" + "id=" + id + ", request=" + request + ", result=" + result + '}';
+    }
+    
+    
+}
