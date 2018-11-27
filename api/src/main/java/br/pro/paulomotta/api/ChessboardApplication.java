30c8f3d33846ad06b9a40a0ef2e5cf9b63efb247
Paulo Motta <prmottajr@gmail.com>
2018-11-27 13:23:11 -0200

    Part #4 Added the configurations for autowiring the repository and entities
    --- ChessboardApplication.java ---
    Added the configurations for the repository

--- api/src/main/java/br/pro/paulomotta/api/ChessboardApplication.java
+++ api/src/main/java/br/pro/paulomotta/api/ChessboardApplication.java
@@ -1,13 +1,23 @@
 package br.pro.paulomotta.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
  * @author paulo
  */
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"br.pro.paulomotta.api"})
+@EnableJpaRepositories(basePackages="br.pro.paulomotta.api.repository")
+@EnableTransactionManagement
+@EntityScan(basePackages="br.pro.paulomotta.api.model")
 public class ChessboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChessboardApplication.class, args);
